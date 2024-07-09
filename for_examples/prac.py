30da6e01ade3597fc5c1d3016d8ab2a8df50b4b4
masaki0087 <pcmv2846@yahoo.co.jp>
2024-07-10 00:34:40 +0900

    a

--- for_examples/prac.py
+++ for_examples/prac.py
@@ -5,7 +5,11 @@ def for1():
         print(i * 2, name)
 
 def for2():
-    numbers = [i % 4 for i in range(1,11)]
+    numbers = [i % 4 for i in range(0,11)]
     print(numbers)
 
-for2()
+def for3():
+    numbers = [i for i in range(0,21) if i < 10 or 18 < i]
+    print(numbers)
+
+for3()
