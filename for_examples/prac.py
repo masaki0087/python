fe2bb392b70c63e9562bf50eee1da4450e394279
masaki0087 <pcmv2846@yahoo.co.jp>
2024-07-10 00:18:37 +0900

    prac.py

---
+++ for_examples/prac.py
@@ -0,0 +1,11 @@
+def for1():
+    fruits = ["apple", "banana", "orange", "grape", "melon"]
+
+    for i, name in enumerate(fruits):
+        print(i * 2, name)
+
+def for2():
+    numbers = [i ** 2 for i in range(1,11)]
+    print(numbers)
+
+for2()
